10f30cb14705dc88564a8b1efea7cda8f8b3e5ea
Danila Simonov <ek@ek-mail.ru>
2023-03-16 18:16:14 +0100

    timestamp lock checks

--- contracts/YlideRegistryV6.sol
+++ contracts/YlideRegistryV6.sol
@@ -89,7 +89,10 @@ contract YlideRegistryV6 is Owned, Terminatable, BlockNumberRingBufferIndex {
         return converted;
     }
 
-    function verifyMessage(bytes32 publicKey, uint8 _v, bytes32 _r, bytes32 _s, uint32 registrar) public view returns (address) {
+    function verifyMessage(bytes32 publicKey, uint8 _v, bytes32 _r, bytes32 _s, uint32 registrar, uint64 timestampLock) public view returns (address) {
+        if (timestampLock > block.timestamp || block.timestamp - timestampLock > 5 * 60 * 1000) {
+            revert('Timestamp lock is invalid');
+        }
         bytes memory prefix = "\x19Ethereum Signed Message:\n330";
         // (121 + 2) + (14 + 64 + 1) + (13 + 8 + 1) + (12 + 64 + 1) + (13 + 16 + 0)
         bytes memory _msg = abi.encodePacked(
@@ -97,7 +100,7 @@ contract YlideRegistryV6 is Owned, Terminatable, BlockNumberRingBufferIndex {
             "Public key: 0x", uint256ToHex(publicKey), "\n",
             "Registrar: 0x", uint32ToHex(bytes4(registrar)), "\n",
             "Chain ID: 0x", uint256ToHex(bytes32(block.chainid)), "\n",
-            "Timestamp: 0x", uint64ToHex(bytes8(uint64(block.timestamp)))
+            "Timestamp: 0x", uint64ToHex(bytes8(timestampLock))
         );
         bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _msg));
         address signer = ecrecover(prefixedHashMessage, _v, _r, _s);
@@ -124,9 +127,9 @@ contract YlideRegistryV6 is Owned, Terminatable, BlockNumberRingBufferIndex {
         internalKeyAttach(msg.sender, publicKey, keyVersion, registrar);
     }
 
-    function attachPublicKeyByAdmin(uint8 _v, bytes32 _r, bytes32 _s, address payable addr, uint256 publicKey, uint32 keyVersion, uint32 registrar, address payable referrer, bool payBonus) external payable onlyBonucer notTerminated {
+    function attachPublicKeyByAdmin(uint8 _v, bytes32 _r, bytes32 _s, address payable addr, uint256 publicKey, uint32 keyVersion, uint32 registrar, uint64 timestampLock, address payable referrer, bool payBonus) external payable onlyBonucer notTerminated {
         require(keyVersion != 0, 'Key version must be above zero');
-        require(verifyMessage(bytes32(publicKey), _v, _r, _s, registrar) == addr, 'Signature does not match the user''s address');
+        require(verifyMessage(bytes32(publicKey), _v, _r, _s, registrar, timestampLock) == addr, 'Signature does not match the user''s address');
         require(referrer == address(0x0) || addressToPublicKey[referrer].keyVersion != 0, 'Referrer must be registered');
         require(addr != address(0x0) && addressToPublicKey[addr].keyVersion == 0, 'Only new user key can be assigned by admin');
 
