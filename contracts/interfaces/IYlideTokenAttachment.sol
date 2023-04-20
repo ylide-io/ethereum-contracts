6fe04f8b7709751b664eee802bc2ae94093eb587
ksamarin90 <konstantin.samarin@roci.fi>
2023-04-20 18:30:03 +0400

    rm unnecessary contract type

--- contracts/interfaces/IYlideTokenAttachment.sol
+++ contracts/interfaces/IYlideTokenAttachment.sol
@@ -4,14 +4,6 @@ pragma solidity ^0.8.17;
 import {IYlideMailer} from "./IYlideMailer.sol";
 
 interface IYlideTokenAttachment {
-	enum ContractType {
-		Pay,
-		Stake,
-		StreamSablier
-	}
-
-	function contractType() external pure returns (ContractType);
-
 	function setYlideMailer(IYlideMailer) external;
 
 	function pause() external;
