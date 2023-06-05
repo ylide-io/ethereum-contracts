0bedf3755e12a633690e3da77cd1b717408f2625
ksamarin90 <konstantin.samarin@roci.fi>
2023-06-05 17:38:56 +0400

    rm scripts

--- scripts/mine.ts
+++
@@ -1,7 +0,0 @@
-import { currentTimestamp, mine } from './utils';
-
-(async () => {
-	await currentTimestamp().then(console.log);
-	await mine(500);
-	await currentTimestamp().then(console.log);
-})();
